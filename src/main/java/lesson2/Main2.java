a6ca021dd1e774e8a04855c63f59f6fa5d3a8391
shaitanshamma <kurbatov_andr@hotmail.com>
2019-08-10 14:16:36 +0300

    Немного улучшений

--- src/main/java/lesson2/Main2.java
+++ src/main/java/lesson2/Main2.java
@@ -7,16 +7,7 @@ import lesson2.array.SortedArrayImpl;
 
 public class Main2 {
     public static void main(String[] args) {
-        final int ITER = 30000;
-//        Integer[] array = new Integer[5];
-//        Integer[] array = new Integer[] {5, 4, 3, 2, 1};
-//        Integer[] array = {5, 4, 3, 2, 1};
-//        array[0] = 5;
-//        displayArray(array);
-//
-//        List<String> arrayList = new ArrayList<>(5);
-
-//        Array<Integer> array = new ArrayImpl<>(5);
+        final int ITER = 50000;
         Array<Double> array = new ArrayImpl<>();
         Array<Double> array2 = new ArrayImpl<>();
         Array<Double> array3= new ArrayImpl<>();
@@ -27,45 +18,27 @@ public class Main2 {
             array2.add(j);
             array3.add(j);
         }
+        System.out.println("Проверка равнозначности массивов до сортировки");
         System.out.println(array.equals(array2) +" " + array.equals(array3) + " "+ array2.equals(array3) );
+        System.out.println("-----");
 //        array.display();
 //        array2.display();
 //        array3.display();
         long start = System.currentTimeMillis();
         array.sortBubble();
         System.out.println("Сортировка пузырьком занимает " + (System.currentTimeMillis() - start) + " мс");
+        System.out.println("-----");
         long start2 = System.currentTimeMillis();
         array2.sortSelect();
         System.out.println("Сортировка выборкой занимает " + (System.currentTimeMillis() - start2) + " мс");
+        System.out.println("-----");
         long start3 = System.currentTimeMillis();
         array3.sortInsert();
         System.out.println("Сортировка вставкой занимает " + (System.currentTimeMillis() - start3) + " мс");
-
-//        array.add(5);
-//        array.add(2);
-//        array.add(4);
-//        array.add(1);
-//        array.add(3);
-//        array.add(8);
-//
-//
-//        array.display();
-////        array.sortBubble();
-////        array.sortSelect();
-//        array.sortInsert();
-//        array.display();
-//
-//        System.out.println("Remove 3:" + array.remove(Integer.valueOf(3)));
-//        System.out.println("Remove 333:" + array.remove(Integer.valueOf(333)));
-//
-//        array.display();
-//
-//        System.out.println("Find 1: " + array.indexOf(1));
-//        System.out.println("Find 5: " + array.indexOf(5));
-//        System.out.println("Find 8: " + array.indexOf(8));
-//        System.out.println("Find 111: " + array.indexOf(111));
-
-
+        System.out.println("-----");
+        System.out.println("Проверка равнозначности массивов после сортировки");
+        System.out.println("-----");
+        System.out.println(array.equals(array2) +" " + array.equals(array3) + " "+ array2.equals(array3) );
     }
 
     private static void displayArray(Integer[] array) {
