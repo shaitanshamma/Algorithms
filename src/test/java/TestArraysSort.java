07dc41673191aeae078137abccb8fb5459de0c7d
shaitanshamma <kurbatov_andr@hotmail.com>
2019-08-10 17:21:57 +0300

    Пробные Тесты

--- src/test/java/TestArraysSort.java
+++ src/test/java/TestArraysSort.java
@@ -1,30 +1,70 @@
-import lesson2.Main2;
 import lesson2.array.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class TestArraysSort {
 
     ArrayImpl<Integer> array;
+    Integer [] expected;
 
     @Before
     public void init(){
-        array = new ArrayImpl<>();
+        array = new ArrayImpl<>(5);
     }
 
     @Test
     public void testAdd(){
+        //array = new ArrayImpl<>(5);
+        expected = new Integer[]{1,2,3,4,5};
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
         array.add(5);
-        array.add(5);
-       // Assert.assertArrayEquals(new int[]{5,4,5}, array.add(5));
-
+        Assert.assertArrayEquals("Данные не совпадают",expected, array.data);
     }
 
     @Test
     public void testRemove(){
-        array.remove(0);
-        Assert.assertArrayEquals(new int[]{4,5}, new int[]{4,5});
-
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+        expected = new Integer[]{1,3,4,5};
+        array.remove(1);
+        Assert.assertArrayEquals("Данные не совпадают",expected, array.data);
     }
+
+    @Test
+    public void testGet(){
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+        Assert.assertEquals((Integer) 3,array.get(4));
+    }
+
+    @Test
+    public void testContains(){
+        array.add(1);
+        array.add(2);
+        Assert.assertTrue("Не верно!",array.contains(1));
+    }
+
+    @Test
+    public void testIndexOf(){
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+        Assert.assertEquals(3,array.indexOf(4));
+    }
+
 }
